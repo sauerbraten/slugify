17a23049553cc30ff041508e6522d30d0caaa5a2
sauerbraten <willing.alexander@googlemail.com>
2014-02-22 14:25:24 +0100

    cleanup; added some replacements

--- slugify.go
+++ slugify.go
@@ -10,42 +10,44 @@ var replacements = map[rune]string{
 	'à':  "a",
 	'ä':  "a",
 	'â':  "a",
+	'ã':  "a",
 	'æ':  "ae",
 	'é':  "e",
 	'è':  "e",
 	'ë':  "e",
 	'ê':  "e",
+	'ẽ':  "e",
 	'í':  "i",
 	'ì':  "i",
-	'i':  "i",
+	'ï':  "i",
 	'î':  "i",
+	'ĩ':  "i",
 	'ó':  "o",
 	'ò':  "o",
 	'ö':  "o",
 	'ő':  "o",
 	'ô':  "o",
 	'ø':  "o",
+	'õ':  "o",
 	'ú':  "u",
 	'ù':  "u",
 	'ü':  "u",
 	'ű':  "u",
+	'û':  "u",
+	'ũ':  "u",
 	'ç':  "c",
+	'ñ':  "n",
 	'ß':  "ss",
-	'·':  "-",
-	'–':  "-",
-	'/':  "-",
-	'_':  "-",
-	',':  "-",
-	':':  "-",
-	';':  "-",
 	'\'': "", // collapses "adam's" to "adams"
 }
 
+// S creates a slug from the str input string. The slug only contains a-z, 0-9 and '-', making it safe to use in URLs.
 func S(str string) string {
-	str = strings.ToLower(strings.TrimSpace(str)) // Trim all whitespace and special characters at beginning and end, then Lower-Case
+	// trim all whitespace and special characters at beginning and end, then convert to lower case
+	str = strings.ToLower(strings.TrimSpace(str))
 	newstr := ""
 
-	// Iterate over string, for each character check if there is a replacement, if so, append it to newstr, if not, append original value to newstr
+	// iterate over string, for each rune check if there is a replacement, if so, append it to newstr, if not, append original value to newstr
 	for _, char := range str {
 		if replacement, ok := replacements[char]; ok {
 			newstr += replacement
@@ -54,10 +56,12 @@ func S(str string) string {
 		}
 	}
 
-	newstr = regexp.MustCompile("[^a-z0-9-]").ReplaceAllString(newstr, "-") // Remove invalid chars (spaces are invalid)
-	newstr = regexp.MustCompile("-+").ReplaceAllString(newstr, "-")         // Collapse dashes
+	// replace invalid chars (spaces are invalid) with dashes
+	newstr = regexp.MustCompile("[^a-z0-9-]").ReplaceAllString(newstr, "-")
 
-	newstr = strings.Trim(newstr, "-") // Trim dashes from beginning and end
+	// collapse dashes ('---' → '-')
+	newstr = regexp.MustCompile("-+").ReplaceAllString(newstr, "-")
 
-	return newstr
+	// trim dashes from beginning and end; return the result
+	return strings.Trim(newstr, "-")
 }
--- slugify_test.go
+++ slugify_test.go
@@ -4,29 +4,26 @@ import (
 	"testing"
 )
 
-// Test Slugify
-func TestSlugify(t *testing.T) {
+func TestS(t *testing.T) {
 	if S("Hello World") != "hello-world" {
 		t.Fail()
 	}
-
-	if S("Hello World69") != "hello-world69" {
-		t.Fail()
-	}
-
 	if S("Hello           World????????") != "hello-world" {
 		t.Fail()
 	}
 
-	if S("aáäâeéëeiíiîoóöőôuúüűunçß·/_,:;") != "aaaaeeeeiiiiooooouuuuuncss" {
-		t.Fail()
-	}
-
-	if S("adam's song") != "adams-song" {
-		t.Fail()
-	}
-
 	if S("what's my name again?") != "whats-my-name-again" {
+		t.Fail()
+	}
+
+	// all runes-to-be-replaced are contained here
+	// to generate:
+	//	s := ""
+	//	for r, _ := range replacements {
+	//		s += string(r)
+	//	}
+	//	fmt.Println(s)
+	if S("äìĩőôüűũãëẽóûâêòúáéùñàèøïçß'æîöíõ") != "aiioouuuaeeouaeouaeunaeoicssaeioio" {
 		t.Fail()
 	}
 }
